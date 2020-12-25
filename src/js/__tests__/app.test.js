e38bfaee99eb4a7daf8a279c9aeff7978e6bf061
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2020-12-25 22:58:26 +0300

    Tests works

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,17 @@
+import checkHealthStatus from '../app';
+
+
+test('should be healthy status', () => {
+  const healthStatus = checkHealthStatus({ name: 'Dima', health: 75 });
+  expect(healthStatus).toBe('healthy');
+});
+
+test('should be wounded status', () => {
+  const healthStatus = checkHealthStatus({ name: 'Dima', health: 34 });
+  expect(healthStatus).toBe('wounded');
+});
+
+test('should be critical status', () => {
+  const healthStatus = checkHealthStatus({ name: 'Dima', health: 4 });
+  expect(healthStatus).toBe('critical');
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+// eslint-disable-next-line consistent-return
+export default function checkHealthStatus({ health }) {
+  if (health > 50) {
+    return 'healthy';
+  }
+  if (health <= 50 && health >= 15) {
+    return 'wounded';
+  }
+  if (health < 15) {
+    return 'critical';
+  }
+  // Тест показывает, что 100% строк покрыто и одновременно с этим 9 строка не покрыта, странно)
+}
